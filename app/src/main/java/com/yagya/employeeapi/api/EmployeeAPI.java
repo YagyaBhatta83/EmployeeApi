6b10b604f4020703e6257fdce86ac25e4c191399
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-23 14:35:45 +0545

    user registered successfully.

--- app/src/main/java/com/yagya/employeeapi/api/EmployeeAPI.java
+++ app/src/main/java/com/yagya/employeeapi/api/EmployeeAPI.java
@@ -1,16 +1,22 @@
 package com.yagya.employeeapi.api;
 
 import com.yagya.employeeapi.model.Employee;
+import com.yagya.employeeapi.model.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
     @GET("employees")
     Call<List<Employee>> getAllEmployees();
+
+    @POST("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
 
     @GET("employee/{empID}")
     Call<Employee> getEmployeeByID(@Path("empID") int empID);
