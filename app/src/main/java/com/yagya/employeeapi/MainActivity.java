d8f1960bbb1822fc0fded87cbb2621e113306f0f
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-23 11:20:26 +0545

    data imported from web with the help of api in our app

--- app/src/main/java/com/yagya/employeeapi/MainActivity.java
+++ app/src/main/java/com/yagya/employeeapi/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnShowAllEmployee, btnRegisterEmployee, btnSearchEmployee, btnUpdateDeleteEmployee;
+    TextView tvoutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnRegisterEmployee = findViewById(R.id.btnRegisterEmployee);
         btnSearchEmployee = findViewById(R.id.btnSearchEmployee);
         btnUpdateDeleteEmployee = findViewById(R.id.btnUpdateDeleteEmployee);
+        tvoutput=findViewById(R.id.tvOutput);
 
 
         btnShowAllEmployee.setOnClickListener(new View.OnClickListener() {
